999fc9ce47bae961412013112f21a06dbf36da53
Benjamin Naidoo <naidoobenjamin1313@gmail.com>
2026-08-12 20:03:15 +0200

    Uploaded Foreach Project Files

---
+++ ForEachLoopsHomeworkApp/ForEachLoopsHomework/Program.cs
@@ -0,0 +1,20 @@
+﻿
+string? data = null;
+List<string> firstNames = new();
+
+do
+{
+    Console.Write("Enter first name (Type exit to quit): ");
+    data = Console.ReadLine();
+
+    if (!String.IsNullOrEmpty(data) && data.ToLower() != "exit")
+    {
+        firstNames.Add(data);
+        Console.WriteLine("Addded...");
+    }
+} while (data.ToLower() != "exit");
+
+foreach (string firstName in firstNames)
+{
+    Console.WriteLine($"Hello {firstName}");
+}
